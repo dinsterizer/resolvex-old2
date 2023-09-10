c2b5e61ce036b2f13353757c8417c63fa658da41
D I V N . D E V <me@divn.dev>
2023-09-10 22:45:48 +0700

    done

---
+++ apps/docs/tailwind.config.ts
@@ -0,0 +1,27 @@
+import type { Config } from 'tailwindcss'
+import defaultTheme from 'tailwindcss/defaultTheme'
+import scrollbarPlugin from 'tailwind-scrollbar'
+import { getIconCollections, iconsPlugin } from '@egoist/tailwindcss-icons'
+import containerQueriesPlugin from '@tailwindcss/container-queries'
+
+
+export default <Partial<Config>> {
+  content: [
+    'src/**/*.*',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+        title: ['Space Grotesk', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    scrollbarPlugin({ nocompatible: true }),
+    iconsPlugin({
+      collections: getIconCollections(['heroicons', 'mdi']),
+    }),
+    containerQueriesPlugin,
+  ],
+}
