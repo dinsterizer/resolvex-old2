5adee4eb97ed38bcfbf674f62e4f16363b009fcc
D I V N . D E V <me@divn.dev>
2023-09-13 09:41:16 +0700

    fix schema

--- apps/api/migrations/0000_luxuriant_cammi.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `users` (
-	`id` text NOT NULL,
-	`name` text NOT NULL,
-	`email` text NOT NULL,
-	`otp` text,
-	`created_at` integer NOT NULL
-);
--- apps/api/src/schema.ts
+++ apps/api/src/schema.ts
@@ -4,9 +4,9 @@ import { datetime, emailCol, json } from './schema.extend'
 import { generateUserId } from './utils'
 
 export const Users = sqliteTable('users', {
-  id: text('id').notNull().$defaultFn(() => generateUserId()),
+  id: text('id').notNull().primaryKey().$defaultFn(() => generateUserId()),
   name: text('name').notNull(),
-  email: emailCol('email').notNull(),
+  email: emailCol('email').notNull().unique(),
   otp: json(object({
     code: string(),
     expiresAt: date(),
