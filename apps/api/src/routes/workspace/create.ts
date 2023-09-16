f4f64fce13d7bf877f68ba4ae66f8cc1d5ca8e3a
Dinh · divn.dev <me@divn.dev>
2023-09-16 16:15:28 +0700

    wip

--- apps/api/src/routes/workspace/create.ts
+++ apps/api/src/routes/workspace/create.ts
@@ -1,9 +1,9 @@
 import { z } from 'zod'
-import { WorkspaceMembers, Workspaces } from '../../schema'
+import { Users, WorkspaceMembers, Workspaces } from '../../schema'
 import { authedProcedure } from '../../trpc'
 
 export const workspaceCreateRouter = authedProcedure
-  .input(z.object({ name: z.string().min(1).max(100) }))
+  .input(z.object({ name: z.string().min(1).max(100), withDemoData: z.boolean().default(false) }))
   .mutation(async ({ ctx, input }) => {
     const workspace = await ctx.db
       .insert(Workspaces)
@@ -20,6 +20,49 @@ export const workspaceCreateRouter = authedProcedure
       workspaceId: workspace.id,
       role: 'admin',
     })
+
+    if (input.withDemoData) {
+      const getDemoUser = async (name: string) => {
+        const email = name.toLowerCase().replace(' ', '.') + '+demo@example.com'
+        const user = await ctx.db.query.Users.findFirst({
+          columns: {
+            id: true,
+            name: true,
+            email: true,
+          },
+          where(t, { eq }) {
+            return eq(t.email, email)
+          },
+        })
+
+        if (user) {
+          return user
+        }
+
+        return ctx.db
+          .insert(Users)
+          .values({
+            name,
+            email,
+          })
+          .returning({
+            id: Users.id,
+            name: Users.name,
+            email: Users.email,
+          })
+          .get()
+      }
+
+      const fakeUsers = [await getDemoUser('John Doe'), await getDemoUser('Mary Jane'), await getDemoUser('Bob Smith')]
+
+      for (const user of fakeUsers) {
+        await ctx.db.insert(WorkspaceMembers).values({
+          userId: user.id,
+          workspaceId: workspace.id,
+          role: 'basic_member',
+        })
+      }
+    }
 
     return {
       id: workspace.id,
